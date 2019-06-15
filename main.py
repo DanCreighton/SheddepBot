f9c1816224f0099fccce2b734ba7bd6cbc5cb90b
Dan <dancreightonn@gmail.com>
2019-06-15 20:50:53 +0100

    Expand error handling to cater for multiple developer user IDs

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import json
 import discord
 from discord.ext import commands
 
-description = '''Sheddep Bot is a Discord bot built upon the Pydest API wrapper for Destiny 2. It's main purpose is to assist in the management of the Sheddep clan community and provide a more on-demand way to deliver in-game information to its members.'''
-
 # Specify the extensions (cogs) to load when the bot starts up
 startup_extensions = ["cogs.admin"]
 
-bot = commands.Bot(command_prefix="/", description=description)
+# Specify the users to notify about potential errors
+developer_user_ids = [226030198095740929]
+
+# Specify the bot's description
+description = '''Sheddep Bot is a Discord bot built upon the Pydest API wrapper for Destiny 2. It's main purpose is to assist in the management of the Sheddep clan community and provide a more on-demand way to deliver in-game information to its members.'''
+
+# Initiate the bot
+bot = commands.Bot(command_prefix="//", description=description)
 
 # Run this code when an error occurs
 @bot.event
 async def on_command_error(ctx, error):
     err = type(error).__name__
     msg = error
-    # Message Dan about the error
-    await bot.get_user(226030198095740929).send(f"Error in **{ctx.guild.name}** #{ctx.channel.name} ({err})\n\n{msg}\n\nOriginal message:\n{ctx.message.author} — *\"{ctx.message.content}\"*")
+    # Message developers about the error
+    for u in developer_user_ids:
+        await bot.get_user(u).send(f"Error in **{ctx.guild.name}** #{ctx.channel.name} ({err})\n\n{msg}\n\nOriginal message:\n{ctx.message.author} — *\"{ctx.message.content}\"*")
     # Notify the user about it
     await ctx.send("Apologies, Guardian, there was an error with your request. A report has been sent to the developers.")
 
