87107adca493a57a05c410216dc823b664482968
Dan <dancreightonn@gmail.com>
2019-06-26 00:17:05 +0100

    Add extra error handling for timezone command

--- cogs/general.py
+++ cogs/general.py
@@ -19,31 +19,39 @@ class General(commands.Cog):
         now = arrow.utcnow()
         # Check if the user provided the abbr (timezone abbreviation) parameter
         if abbr != None:
-            # If a time wasn't provided
-            if tztime == None:
-                # Initialise the appropriate lists
-                names = []
-                times = []
-                # Iterate through the timezone data
-                for timezone in tzdata:
-                    # If the provided abbr parameter exists in the timezone data
-                    if abbr.upper() == timezone['abbr']:
+            # Initialise the appropriate lists
+            names = []
+            times = []
+            # If the provided abbr parameter exists in the timezone data
+            if any(tz.get('abbr') == abbr.upper() for tz in tzdata):
+                # Iterate over the timezone data
+                for tz in tzdata:
+                    if abbr.upper() == tz['abbr']:
                         # Add the timezone name to a list
-                        names.append(timezone['value'])
+                        names.append(tz['value'])
                         # Shift the UTC time by the corresponding offset
-                        times.append(now.shift(hours=timezone['offset']))
-                # Check if there are multiple timezones found
-                if len(times) > 1:
-                    # Construct the message
-                    msg = f"There are several times listed under **{abbr.upper()}**:"
-                    for t in range(len(times)):
-                        msg += f"\nThe current time in **{names[t]}** is {times[t].format('h:mma')}."
-                    # Send message to channel
-                    await ctx.send(msg)
-                # Otherwise
+                        times.append(now.shift(hours=tz['offset']))
+                # If a time was provided
+                if tztime != None:
+                    pass
+                # If a time wasn't provided
                 else:
-                    # Send message to channel
-                    await ctx.send(f"The current time in **{names[0]}** is {times[0].format('h:mma')}.")
+                    # If there are multiple timezones found
+                    if len(times) > 1:
+                        # Construct the message
+                        msg = f"There are several times listed under **{abbr.upper()}**:"
+                        for t in range(len(times)):
+                            msg += f"\nThe current time in **{names[t]}** is {times[t].format('h:mma')}."
+                        # Send message to channel
+                        await ctx.send(msg)
+                    # If only one timezone is found
+                    else:
+                        # Send message to channel
+                        await ctx.send(f"The current time in **{names[0]}** is {times[0].format('h:mma')}.")
+            # If the provided abbr parameter doesn't exist in the timezone data
+            else:
+                # Send message to channel
+                await ctx.send("That timezone isn't valid, Guardian. Please specify a different one.")
         # If the timezone abbreviation wasn't provided
         else:
             # Send message to channel
